cc3bbd3b37545f0fdae00ad4f9e4247f257c734e
sgriffin10 <sgriffin4@babson.edu>
2020-01-31 22:36:05 -0500

    exercises updated

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int val;
     int x = 5;
     int y = x + 1;
     printf("Hello, World!\n");
     printf("y is %d\n",y);
+    val = RandomValue(3);
+    printf(val);
     return 0;
+}
+
+void RandomValue(int ){
+    return 69;
 }
 
 /*
