7b06858d0d789f9ce4b816676a7cc1d84e7b287a
sgriffin10 <sgriffin4@babson.edu>
2020-02-06 02:23:40 -0500

    Create adder.c

---
+++ exercises/ex02/adder.c
@@ -0,0 +1,25 @@
+/*Create a file named `adder.c` and write a program that allows the user to enter integers, one per line, until the user hits Control-D, which is the end-of-file (EOF) character.
+
+It should convert the numbers from input strings to integers and store the integers in an array.  The program should produce appropriate error messages if the user enters something that exceeds the length of the input buffer, or can't be converted to an integer.  It should also produce an error message if the user enters more numbers than will fit in the array.
+
+After the user enters Control-D, the program should add up the numbers in the array and print the result.  
+
+Your program should be divided into at least two functions with well designed interfaces.
+
+Optionally, the program should produce an error if the sum exceeds the largest integer that can be represented.
+
+Hint: You will probably want to use two functions we haven't seen yet:
+
+* strlen: [Read about it here](https://www.tutorialspoint.com/c_standard_library/c_function_strlen.htm)
+
+* atoi: [Read about it here](https://www.tutorialspoint.com/c_standard_library/c_function_atoi.htm)
+
+Before you use their functions, you need to `#include <string.h>`. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    
+}
